12ab75fd9e47b1189e2ee0eb371f1d1871c8bdcd
AcademiccMacharia <machariab19@gmail.com>
2024-07-23 16:31:05 +0300

    [feat]: implementing features section

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+//...
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://1055c2393210fd6e29d6492d92f6651e@o4507650700083200.ingest.us.sentry.io/4507650711224320",
   integrations: [
     Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
   ],
   tracesSampleRate: 1.0, 
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1, 
-  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
 });
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
